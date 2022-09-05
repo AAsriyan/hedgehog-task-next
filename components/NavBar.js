620365053ef05d2ed0b082c46a037c676a82b8f7
Arshak Asriyan <aasriyan01@gmail.com>
2022-09-05 15:37:33 -0400

    created the ability for a user to connect to the phantom wallet

---
+++ components/NavBar.js
@@ -0,0 +1,11 @@
+import styles from "../styles/NavBar.module.css";
+
+const NavBar = () => {
+	return (
+		<div className={styles.navbar}>
+			<h1>Solana Wallet Task</h1>
+		</div>
+	);
+};
+
+export default NavBar;
